9a1058faef88f98cd00d37b3180ed8c71ee413c1
Adam Hradil <hradiladam@gmail.com>
2024-11-19 19:49:53 +0100

    commit 4-add JS code to fetch quotes from a selectionOfQuotes.txt file located in 'assets' folder

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 // Array with selected quotes
 
-const quoteSelection = [
-    `Richard Feynman (Physicist): “The first principle is that you must not fool yourself, and you are the easiest person to fool.”`,
-    `Maya Angelou (Poet and Activist): “Do the best you can until you know better. Then when you know better, do better.”`,
-    `Socrates (Philosopher): “The only true wisdom is in knowing you know nothing.”`,
-    `Oscar Wilde (Playwright and Poet): “Be yourself; everyone else is already taken.”`,
-    `Heraclitus (Philosopher): “No man ever steps in the same river twice, for it's not the same river and he's not the same man.”`,
-    `Richard Feynman (Physicist): “I would rather have questions that can't be answered than answers that can't be questioned.”`,
-    `Neil Degrasse Tyson (Astrophysicist): “We are stardust brought to life, then empowered by the universe to figure itself out — and we have only just begun.”`,
-    `George Bernard Shaw (Playwright): “Life isn't about finding yourself. Life is about creating yourself.”`,
-    `Rober Downey Jr. (Actor): "Listen, smile, agree and then do whatever the fuck you were going to do anyway."`,
-    `Isaac Asimov (Writer and Biochemist): “The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.”`,
-    `Leonardo da Vinci (Polymath): “Simplicity is the ultimate sophistication.”`,
-    `Voltaire (Philosopher): “Judge a man by his questions rather than his answers.”`,
-    `James Baldwin (Writer and Activist): “Not everything that is faced can be changed, but nothing can be changed until it is faced.”`
-];
+let quoteSelection = [];
 
+
+// importing data from a txt file
+
+const fetchData = async () => {
+    try {
+        const response = await fetch('../assets/quoteSelection.txt');
+        if (!response.ok) {
+            throw new Error ('Response was not ok. Please check the file path.');
+        }
+        const data = await response.text();
+        quoteSelection = data.split('\n');
+        console.log(quoteSelection);
+    } catch (error) {
+        console.error('There was a problem fetching data:', error);
+    }
+}
+
+fetchData();
 
 
 // Random quote generator
