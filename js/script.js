5d2fc0f530ed6b07a3cebb3f77bba9c6eadd3bf7
Adam Hradil <hradiladam@gmail.com>
2024-11-19 23:38:43 +0100

    Commit 5-rename .txt source to "quotes" and update JS script for processing and filtering text from quotes.txt

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
-// Array with selected quotes
+let selectionOfQuotes = [];
 
-let quoteSelection = [];
-
-
-// importing data from a txt file
+/*
+Prepares import of quotes from a txt file,
+normalizes curly quotes into straight quotes,
+filteres away lines that initially do not include a colon,
+splits the text into author and quote, 
+trims whitespaces around author and quote,
+adds the colon (:) back to author,  
+*/
 
 const fetchData = async () => {
     try {
-        const response = await fetch('../assets/quoteSelection.txt');
+        const response = await fetch('../assets/quotes.txt');
         if (!response.ok) {
             throw new Error ('Response was not ok. Please check the file path.');
         }
+
         const data = await response.text();
-        quoteSelection = data.split('\n');
-        console.log(quoteSelection);
+        const normalizedData = data.replace(/[\u2018\u2019\u201C\u201D]/g, '"');
+
+        selectionOfQuotes = normalizedData.split('\n').map((line) => {
+
+            if (line.includes(':')) {
+                let [author, quote] = line.split(':');
+
+                author = author.trim();
+                quote = quote.trim();
+
+                if (!quote.startsWith('"')) {
+                    quote = `"${quote}`;
+                }
+                if (!quote.endsWith('"')) {
+                    quote = `${quote}"`;
+                }
+
+                author = `${author}:`; 
+                return {author, quote};
+            }
+            return null;
+
+        }).filter((item) => item !== null); 
+
+        console.log(selectionOfQuotes);
+
     } catch (error) {
         console.error('There was a problem fetching data:', error);
     }
 }
+
 
 fetchData();
 
@@ -31,13 +61,11 @@ const generateQuote = () => {
     let newQuote;
     
     do {
-        randomNum = Math.floor(Math.random() * quoteSelection.length);
-        newQuote = quoteSelection[randomNum];
+        randomNum = Math.floor(Math.random() * selectionOfQuotes.length);
+        newQuote = selectionOfQuotes[randomNum];
     } while (newQuote === lastQuote);
 
-    const [author, quote] = newQuote.split(': ');
-
-    document.getElementById('quote-display').innerHTML = `<span class="author">${author}</span>: <span class="quote">${quote}</span>`;
+    document.getElementById('quote-display').innerHTML = `<span class="author">${newQuote.author}</span> <span class="quote">${newQuote.quote}</span>`;
     lastQuote = newQuote
 };
 
